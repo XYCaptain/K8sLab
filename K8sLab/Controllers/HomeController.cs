c5bb56e4b2e683ce04951d36c2f03690cf9904dc
Mr.XY <10951630+XYCaptain@users.noreply.github.com>
2023-01-12 14:08:43 +0800

    Add project files.

---
+++ K8sLab/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using K8sLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+
+namespace K8sLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var hostname = Dns.GetHostName();
+            ViewBag.HostName = hostname;
+            ViewBag.HostIp = Dns.GetHostAddresses(hostname).FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
